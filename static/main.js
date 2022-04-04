2630c6b6fecbf8d7a20701397f5be2897be391b6
kasiasaif <kasia.saif@gmail.com>
2022-04-04 18:03:50 +0100

    css

--- static/main.js
+++ static/main.js
@@ -1,25 +1,25 @@
 
 
 
-$('#main').toggleClass(localStorage.toggled);
+// $('#main').toggleClass(localStorage.toggled);
 
-function darkLight() {
-  /*DARK CLASS*/
-  if (localStorage.toggled != 'dark') {
-    $('#main, p').toggleClass('dark', true);
-    localStorage.toggled = "dark";
+// function darkLight() {
+//   /*DARK CLASS*/
+//   if (localStorage.toggled != 'dark') {
+//     $('#main, p').toggleClass('dark', true);
+//     localStorage.toggled = "dark";
      
-  } else {
-    $('#main, p').toggleClass('dark', false);
-    localStorage.toggled = "";
-  }
-}
+//   } else {
+//     $('#main, p').toggleClass('dark', false);
+//     localStorage.toggled = "";
+//   }
+// }
 
-/*Add 'checked' property to input if background == dark*/
-if ($('main').hasClass('dark')) {
-   $( '#checkBox' ).prop( "checked", true )
-} else {
-  $( '#checkBox' ).prop( "checked", false )
-}
+// /*Add 'checked' property to input if background == dark*/
+// if ($('main').hasClass('dark')) {
+//    $( '#checkBox' ).prop( "checked", true )
+// } else {
+//   $( '#checkBox' ).prop( "checked", false )
+// }
 
 
